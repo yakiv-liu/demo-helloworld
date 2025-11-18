ff8588329bf5faa9f2a1ca66a18f8ad5f241d54f
Liu Yaqin <liuyq19@asiainfo.com>
2025-11-19 02:31:02 +0800

    Add port and agentLabel

--- jenkinsfiles/JenkinsfileMainAutoDeploy.groovy
+++ jenkinsfiles/JenkinsfileMainAutoDeploy.groovy
@@ -7,7 +7,8 @@ properties([
                 string(name: 'PROJECT_NAME', defaultValue: 'demo-helloworld', description: '项目名称'),
                 string(name: 'PROJECT_REPO_URL', defaultValue: 'git@github.com:yakiv-liu/demo-helloworld.git', description: '项目代码仓库 URL'),
                 string(name: 'PROJECT_BRANCH', defaultValue: 'main', description: '项目代码分支（默认：main）'),
-//                booleanParam(name: 'IS_RELEASE', defaultValue: false, description: '正式发布'),
+                string(name: 'APP_PORT', defaultValue: '8085', description: '应用服务端口号'),
+                string(name: 'AGENT_LABEL', defaultValue: 'docker-jnlp-slave', description: 'Jenkins Agent节点标签'),
                 string(name: 'EMAIL_RECIPIENTS', defaultValue: '251934304@qq.com', description: '邮件接收人'),
                 booleanParam(name: 'SKIP_DEPENDENCY_CHECK', defaultValue: true, description: '跳过依赖检查以加速构建（默认跳过）')
         ])
@@ -19,19 +20,14 @@ mainAutoDeployPipeline([
         projectName: params.PROJECT_NAME,
         projectRepoUrl: params.PROJECT_REPO_URL,
         projectBranch: params.PROJECT_BRANCH,
-        org: 'yakiv-liu',
-        repo: 'demo-helloworld',
-        agentLabel: 'docker-jnlp-slave',
+//        org: 'yakiv-liu',
+//        repo: 'demo-helloworld',
+        agentLabel: params.AGENT_LABEL,
+        appPort: params.APP_PORT.toInteger(),
         defaultEmail: params.EMAIL_RECIPIENTS,
-
-        // 用户选择参数
-//        isRelease: params.IS_RELEASE.toBoolean(),
-
         // 跳过依赖检查参数
         skipDependencyCheck: params.SKIP_DEPENDENCY_CHECK.toBoolean(),
-
         // 项目特定配置
-        appPort: 8085,
         environmentHosts: [
                 staging: [host: '192.168.233.8'],
                 'pre-prod': [host: '192.168.233.9'],
